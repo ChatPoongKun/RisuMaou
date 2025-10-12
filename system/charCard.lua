bb62d7751a424c451c5910d85045c20bee5633a0
Hoon <sera141@naver.com>
2025-10-13 04:19:01 +0900

    조교 시작시 abl이나 보유한 특성에 따라 초기 stat이 변동되도록 함

--- system/charCard.lua
+++ system/charCard.lua
@@ -6,7 +6,7 @@ function(triggerId, name)
         setState(triggerId, "screen", "charInfo") --캐릭터 상세 정보 화면으로 이동
 
     elseif screen == "preTrain" then
-        sysFunction(triggerId, "preTrainSetting")
+        sysFunction(triggerId, "preTrainSetting.sys", name)
         setState(triggerId, "screen", "train") --조교 화면으로 이동
     end
 end
