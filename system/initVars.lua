3bf9d5ee7ddf6617b27294474af4f0adc08b398c
Hoon <sera141@naver.com>
2025-10-21 01:45:42 +0900

    Juel을 별도 state 및 챗변수로 처리 게임 시작 후 퍼메가 지워지므로 addchat으로 html을 출력

--- system/initVars.lua
+++ system/initVars.lua
@@ -9,21 +9,22 @@ function (triggerId)
         difficulty = "보통", --조교난이도
         allowDeath = 0, --사망방지. 기본값 OFF
         debug = DEBUG,
-        Lvtable = getLoreBookContent(triggerId, "Lvtable.db")
     }
 
     for key, value in pairs(initVars) do
         setChatVar(triggerId, key, value)
     end
+
     --user 및 고유캐릭터 정보를 로컬 로어북에 저장
+    local juel = {}
     for _, v in ipairs(json.decode(initVars.chars)) do --리수스타일 배열을 다시 테이블로
         local content = json.decode(getLoreBookContent(triggerId, v..".init")) -- 대상 캐릭터의 로어북을 불러와 테이블로 저장
         --[[
         현재 로직을 별도의 캐릭터 생성로직에 포함하고 해당 함수를 가져와서 쓰는 방식을 고려할것.
         ]]
 
-        --캐릭터에는 정의가 필요한 부분만 넣고 들어가지 않은 trait, juel등의 필드를 db에서 읽어와서 추가
-        local db = {"mark.db", "abl.db", "juel.db", "exp.db"}
+        --캐릭터에는 정의가 필요한 부분만 들어있음. 캐릭터 시트에 없는 trait, juel등의 필드를 db에서 읽어와서 추가
+        local db = {"mark.db", "abl.db", "exp.db"}
         local added = ""
         for _, tbl in pairs(db) do
             for key,_ in pairs(json.decode(getLoreBookContent(triggerId, tbl))) do
@@ -38,7 +39,15 @@ function (triggerId)
         --캐릭터 정보를 로컬로어북에 저장
         local option = {alwaysActive = false, insertOrder = 100, key = "", secondKey = "", regex = false}
         upsertLocalLoreBook(triggerId, v, json.encode(content), option)
+
+        --주얼은 별도의 state로 저장
+        local jueldb = json.decode(getLoreBookContent(triggerId, "juel.db"))
+        juel[v] = {}
+        for j, _ in pairs(jueldb) do
+            juel[v][j] = 0
+        end
     end
+    setState(triggerId, "juel", juel)
 
     --유저 정보를 user변수에 테이블로 할당
     local user = json.decode(getLoreBookContent(triggerId, "user"))
@@ -71,7 +80,6 @@ function (triggerId)
     juel_tbl = string.gsub(juel_tbl, ",$", "]")-- 마지막 쉼표 대신 괄호 닫기
     setChatVar(triggerId, "juelList", juel_tbl)
 
-
     --trait.db에서 각 카테고리의 모든 항목 키값을 각각의 카테고리명 챗변수로 저장
     local trait = json.decode(getLoreBookContent(triggerId, "trait.db"))
     for cat, tbl in pairs(trait) do
@@ -83,4 +91,6 @@ function (triggerId)
         cat_tbl = string.gsub(cat_tbl, ",$", "]")-- 마지막 쉼표 대신 괄호 닫기
         setChatVar(triggerId, cat.."List", cat_tbl)
     end
+
+    addChat(triggerId, "user", "{{".."getvar::html".."}}")
 end
