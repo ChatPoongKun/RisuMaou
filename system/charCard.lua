158b39f34d0071d7f909749fa906f2bebe635ed7
Hoon <sera141@naver.com>
2025-10-21 02:44:25 +0900

    변경된 juel 저장로직에 맞게 캐릭터 화면에서 juel이 조회되도록 수정

--- system/charCard.lua
+++ system/charCard.lua
@@ -1,16 +1,20 @@
 function(triggerId, name)
     debug("charCard 실행")
     local screen = getState(triggerId, "screen")
-    if screen == "charList" then
+    if screen == "charList" then --캐릭터 조회화면
         if name == getPersonaName(triggerId) then
             name = "user"
         end
         local target = json.decode(getLoreBookContent(triggerId, name))
         stateToVar(triggerId, "target", target)
+        local targetJuel = getState(triggerId, "juel")[name]
+        stateToVar(triggerId, "targetJuel", targetJuel)
         setState(triggerId, "screen", "charInfo") --캐릭터 상세 정보 화면으로 이동
 
-    elseif screen == "preTrain" then
+    elseif screen == "preTrain" then --조교 대상 화면
         sysFunction(triggerId, "preTrainSetting.sys", name)
+        local targetJuel = getState(triggerId, "juel")[name]
+        stateToVar(triggerId, "targetJuel", targetJuel)
         setState(triggerId, "screen", "train") --조교 화면으로 이동
     end
 end
