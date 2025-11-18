b90133a72a0cf9071f13c66edcc7a68308c96ff8
Hoon <sera141@naver.com>
2025-11-18 21:07:29 +0900

    마계사전 UI초안 작성

--- Pages/dictionary/dictionary.lua
+++ Pages/dictionary/dictionary.lua
@@ -1,4 +1,16 @@
+[dictionary/d_abl/hidden] function(triggerId)
+    local dictCat = "d_abl"
+    setChatVar(triggerId, "category", dictCat)
+end!!
+
+[dictionary/d_trait/hidden] function(triggerId)
+    local dictCat = "d_trait"
+    setChatVar(triggerId, "category", dictCat)
+end!!
+
+
+
 [dictionary/199/돌아가기] function(triggerId)
     setChatVar(triggerId, "category", "items")
     setState(triggerId, "screen", "main")
-end!!
+end!!   
